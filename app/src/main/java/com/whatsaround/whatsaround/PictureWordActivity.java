dce4349fff26d21861c2dbfa18d90fd938ad8cac
Kyle Kravette <kkravette@gmail.com>
2015-03-31 01:11:09 -0400

    Fixed picture orientation issue

--- app/src/main/java/com/whatsaround/whatsaround/PictureWordActivity.java
+++ app/src/main/java/com/whatsaround/whatsaround/PictureWordActivity.java
@@ -1,11 +1,13 @@
 package com.whatsaround.whatsaround;
 
 import android.app.Activity;
+import android.content.CursorLoader;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
+import android.media.ExifInterface;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.os.Bundle;
@@ -14,6 +16,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.whatsaround.whatsaround.com.whatsaround.whatsaround.dataType.PictureWord;
+
+import java.io.IOException;
 
 
 public class PictureWordActivity extends Activity {
@@ -30,25 +36,36 @@ public class PictureWordActivity extends Activity {
         Intent intent = getIntent();
         String word = intent.getStringExtra("word");
         Uri uri = Uri.parse(intent.getStringExtra("uri"));
+        String path = getRealPathFromURI(uri);
 
-        String[] filePath = {MediaStore.Images.Media.DATA};
-        Cursor cursor = getContentResolver().query(uri, filePath, null, null, null);
-        cursor.moveToFirst();
-        int index = cursor.getColumnIndex(filePath[0]);
-        String picPath = cursor.getString(index);
-        Log.d(ACTIVITY, "The location of the photo is: " + picPath);
-        cursor.close();
-        Bitmap bitmap = BitmapFactory.decodeFile(picPath);
-        Matrix matrix = new Matrix();
-        matrix.postRotate(90);
-        Bitmap bit = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
-
+        Log.d(ACTIVITY, "The location of the photo is: " + path);
+        int orientation = getExifOrientation(path);
+        Bitmap bitmap = BitmapFactory.decodeFile(path);
+        int rotate;
+        switch(orientation){
+            case 90: rotate = 90;
+                     break;
+            case 180: rotate = 180;
+                     break;
+            case 270: rotate = 270;
+                     break;
+            default: rotate = 0;
+                     break;
+        }
+        if(rotate != 0){
+            Matrix matrix = new Matrix();
+            matrix.postRotate(90);
+            Bitmap bit = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+            ImageView image = (ImageView)findViewById(R.id.picture);
+            image.setImageBitmap(bit);
+        }
+        else {
+            ImageView image = (ImageView)findViewById(R.id.picture);
+            image.setImageBitmap(bitmap);
+        }
 
         TextView tv = (TextView)findViewById(R.id.label);
         tv.setText(word);
-
-        ImageView image = (ImageView)findViewById(R.id.picture);
-        image.setImageBitmap(bit);
     }
 
 
@@ -76,5 +93,42 @@ public class PictureWordActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public static int getExifOrientation(String filepath) {// YOUR MEDIA PATH AS STRING
+        int degree = 0;
+        ExifInterface exif = null;
+        try {
+            exif = new ExifInterface(filepath);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        if (exif != null) {
+            int orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, -1);
+            if (orientation != -1) {
+                switch (orientation) {
+                    case ExifInterface.ORIENTATION_ROTATE_90:
+                        degree = 90;
+                        break;
+                    case ExifInterface.ORIENTATION_ROTATE_180:
+                        degree = 180;
+                        break;
+                    case ExifInterface.ORIENTATION_ROTATE_270:
+                        degree = 270;
+                        break;
+                }
+
+            }
+        }
+        return degree;
+    }
+
+    private String getRealPathFromURI(Uri contentUri) {
+        String[] proj = { MediaStore.Images.Media.DATA };
+        CursorLoader loader = new CursorLoader(this, contentUri, proj, null, null, null);
+        Cursor cursor = loader.loadInBackground();
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        return cursor.getString(column_index);
     }
 }
