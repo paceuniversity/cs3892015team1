a196ceca0c2c2f4ca2991d16cec79c53a46d2d46
Luiz Henrique <lpcluizhenrique@gmail.com>
2015-04-25 15:45:15 -0400

    Changes database class name from FlashcardsDBHelper to DatabaseHelper. Modifies its implementation to the one that is in the design branch (in the DatabaseHelper class). The class QuestionsContract is also added. It is a class that establish the names of the tables and columns, in order to separate this specifications from the code that will create and change the database.

---
+++ app/src/main/java/com/whatsaround/whatsaround/data/QuestionsContract.java
@@ -0,0 +1,14 @@
+package com.whatsaround.whatsaround.data;
+
+
+public final class QuestionsContract {
+
+    public static final String TABLE_NAME = "question";
+
+    public static final class Columns {
+        public static final String ID = "_id";
+        public static final String ANSWER = "name";
+        public static final String IMAGE_PATH = "image_path";
+    }
+
+}
